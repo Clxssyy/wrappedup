bb21bc079f21bb1deb24db8846bd1dcbec1eeded
Michael Connolly <mpc63@uakron.edu>
2023-09-17 18:53:37 -0400

    Update login screen

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,18 +1,22 @@
-'use client';
+'use client'
 
-import { getProviders, signIn } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
+import Image from 'next/image';
 
-export default async function Login() {
-  const providers = await getProviders();
+export default function Login() {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id, { callbackUrl: '/' })}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
+      <section className='grow flex flex-col gap-2 place-items-center justify-center bg-zinc-900'>
+        <Image
+          src='spotify_icon.svg'
+          alt='Spotify Logo'
+          width={200}
+          height={200}
+        />
+        <button className='bg-green-500 p-2 rounded-lg text-lg' onClick={() => signIn("spotify", { callbackUrl: '/' })}>
+          Sign in with Spotify
+        </button>
+      </section>
     </>
   );
 }
