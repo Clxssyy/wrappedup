b7c14e91689081519decc9948703bd78fbb5738f
Michael Connolly <mpc63@uakron.edu>
2024-07-19 22:23:30 -0400

    Change file to jsx temporarily

---
+++ src/app/user/[name]/page.jsx
@@ -0,0 +1,5 @@
+const ProfilePage = (params) => {
+  return <div>{params.name}</div>;
+};
+
+export default ProfilePage;
--- src/app/user/[name]/page.tsx
+++
@@ -1,5 +0,0 @@
-const ProfilePage = ({ params }: { params: { name: string } }) => {
-  return <div>{params.name}</div>;
-};
-
-export default ProfilePage;
